f45638b55915050e30ce2238804fac0460b08547
dantaya <dantaya90@gmail.com>
2019-09-13 11:50:37 -0400

    Fixed ip reading.

--- ip.go
+++ ip.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 )
 
+// IP details about the IP address
 type IP struct {
 	Address string // Original/full notation passed in
 	Parts   []string
@@ -16,10 +17,17 @@ type IP struct {
 	CIDR    int64
 }
 
+// ReadUserIP gets the users IP address from the request
+// @TODO: fix this to handle IPS properly building on logic from https://husobee.github.io/golang/ip-address/2015/12/17/remote-ip-go.html
 func ReadUserIP(r *http.Request) string {
-	IPAddress := r.Header.Get("X-Real-Ip")
+
+	IPAddress := r.Header.Get("X-Forwarded-For")
+	if IPAddress != "" {
+		IPS := strings.Split(IPAddress, ",")
+		IPAddress = IPS[0]
+	}
 	if IPAddress == "" {
-		IPAddress = r.Header.Get("X-Forwarded-For")
+		IPAddress = r.Header.Get("X-Real-IP")
 	}
 	if IPAddress == "" {
 		IPAddress, _, _ = net.SplitHostPort(r.RemoteAddr)
